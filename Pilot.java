92e2168d7169af5def475ba62061fc344fc922cc
EduPuja <edpufa@inspalamos.cat>
2022-03-18 19:10:08 +0100

    Objecte pilot ,serveix per fer el prorgrama

---
+++ Pilot.java
@@ -0,0 +1,77 @@
+//package Registres;
+import java.time.LocalDate;
+/**
+
+Classe pilot
+*/
+public class Pilot
+{
+        // hereadar de Equip
+        private String nom;
+        //private String nomEquip;
+        private LocalDate dataNaix;
+        private int puntuacio;
+
+        /**
+         * Construcotr
+         */
+        public Pilot()
+        {
+
+        }
+        
+        /**
+         * Metode per entrar tot els metadatos d'un pilot
+         * @param nomEntrat
+         * @param dataNaix
+         * @param punts
+         */
+
+        public void setTotPilot(String nomEntrat,LocalDate dataNaix,int punts)
+        {
+         this.nom = nomEntrat;
+         this.dataNaix =dataNaix;
+         this.puntuacio = punts;
+        }
+        
+        /**
+         * metode que et retorna el nom - String
+         * @return
+         */
+        
+        public String getNom()
+        {
+        	return this.nom;
+        }
+        
+        /**
+         * Metode que et retorna la data de naixament LOCALDATE
+         * @return
+         */
+        public LocalDate getDataNaix()
+        {
+        	return this.dataNaix;
+        }
+        
+        
+        
+        public int getPunts()
+        {
+        	return this.puntuacio;
+        }
+        
+        /**
+         * Metode per llistar tots els meta datos d'un o més piolts
+         * @return
+         */
+        public String getLlistarPilot()
+        {
+        	String text = "";
+        	
+        	text = "Nom: "+ this.nom +" DataNaix "+ this.dataNaix.getDayOfMonth() +"/"+this.dataNaix.getMonthValue()+"/"+this.dataNaix.getYear()+" PUNTS: "+this.puntuacio;
+        	return text;
+        
+        	
+        }
+
+}
