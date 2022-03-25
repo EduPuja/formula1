b414142e118d0a86d1d0db08f066fd9738565800
EduPuja <edpufa@inspalamos.cat>
2022-03-25 18:56:04 +0100

    object ciruits

--- Circuits.java
+++ Circuits.java
@@ -1,3 +1,64 @@
-public class Circuits {
+public class Circuits 
+{
+    //variables
+    private String nomCircut;
+    private int numCircut;
+
+    // !! variable que encara esta en discusio !!
+    private int numVoltes;
+    /**
+     * Constructor buit 
+     */
+    public Circuits()
+    {
+
+    }
+    /**
+     * Constructor v2 per crear directament un circuit
+     * @param num
+     * @param nom
+     */
+    public Circuits(int num , String nom)
+    {
+        this.nomCircut=nom;
+        this.numCircut=num;
+    }
+    /**
+     * metode que crea un cicuit
+     * @param num
+     * @param nom
+     */
+    public void setCircuit(int num,String nom)
+    {
+        this.nomCircut =nom;
+        this.numCircut = num;
+    }
+    /**
+     * Metode que et retorna tota la informacio de una Circuit
+     * @return text
+     */
+    public String getInfoCiruit()
+    {
+        return "Num_Cicuit: "+this.numCircut +" Nom_Circuit: "+this.nomCircut;
+    }
+
+    /**
+     * metode que et retorna el nom del circuit
+     * @return this.nomCircuit;
+     */
+    public String getNomCircuti()
+    {
+        return this.nomCircut;
+    }
+
+    /**
+     * metode que et retorna 
+     * @return this.numCircuit;
+     */
+    public int getNumCircuit()
+    {
+        return this.numCircut;
+    }
+
     
 }
