769a16ceedbe5a208fe9369fe597c09024291513
EduPuja <edpufa@inspalamos.cat>
2022-04-08 19:02:51 +0200

    canvis Persona

--- Objectes/Persona.java
+++ Objectes/Persona.java
@@ -1,6 +1,34 @@
 package Objectes;
 
+import java.time.LocalDate;
+
 public abstract class Persona 
 {
-        
+    private String dni;
+    private String nom;
+    private LocalDate dataNaix;
+
+    /**
+     * Metode que serveix per entrar una persona
+     * @param dniEntrat
+     * @param nomEntrat
+     * @param dataNaix
+     */
+    public void setPersona(String dniEntrat,String nomEntrat,LocalDate dataNaix)
+    {
+        this.dni = dniEntrat;
+        this.nom =nomEntrat;
+        this.dataNaix = dataNaix;
+    }
+
+    /**
+     * metode que et reotrna tota la info d'una persona
+     * @return
+     */
+    public String getInfoPersona()
+    {
+        return this.dni +" "+this.nom +" "+this.dataNaix.getDayOfMonth()+"/"+this.dataNaix.getMonthValue()+"/"+this.dataNaix.getYear();
+    }
+
+
 }
